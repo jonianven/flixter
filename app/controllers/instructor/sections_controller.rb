dc68a35e51cd1a751cac11aa410861b60f09fd7a
Jonamir Ian Ventura <jonianven@gmail.com>
2016-05-11 06:49:04 +0000

    Reordering sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,6 +11,11 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end 
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end 
+
   private
 
   def require_authorized_for_current_course
@@ -19,12 +24,20 @@ class Instructor::SectionsController < ApplicationController
     end 
   end 
 
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end 
+
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end 
   end 
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end 
 end
